3e116bca6f570a9cf2be15626911469dd57fc7e6
Jamal Izabakarov <jizabakarov8@gmail.com>
2026-04-02 18:17:44 +0400

    Add DTOs

--- src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryRequestDto.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryRequestDto.java
@@ -1,21 +1,15 @@
 package az.edu.ada.wm2.lab6.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryRequestDto {
 
     private String name;
-
-    public CategoryRequestDto() {
-    }
-
-    public CategoryRequestDto(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryResponseDto.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryResponseDto.java
@@ -1,33 +1,18 @@
 package az.edu.ada.wm2.lab6.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryResponseDto {
 
     private UUID id;
     private String name;
-
-    public CategoryResponseDto() {
-    }
-
-    public CategoryResponseDto(UUID id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
